a103287aa05d5dc7cd8c58030a7ee0c190f09760
tran phuoc <tranphuoc@trans-MacBook-Pro.local>
2022-06-20 17:15:50 +0700

    add nvim configs

---
+++ nvim/init.lua
@@ -0,0 +1,53 @@
+local fn = vim.fn
+local execute = vim.api.nvim_command
+
+local function packer_init()
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+        execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+    end
+    -- vim.cmd("packadd packer.nvim")
+    vim.cmd("autocmd BufWritePost plugins.lua PackerCompile")
+end
+
+packer_init()
+
+-- general
+require("config.autocmd").setup()
+require("config").setup()
+require("plugins").setup()
+require("config.keymappings").setup()
+
+-- ext
+require("ext").setup()
+
+-- lsp config
+require("lsp").setup()
+require("ext.cmp").setup()
+
+-- code formatter
+require("ext.formatters").setup()
+
+-- lsp server
+require("lsp.bash").setup()
+require("lsp.cc").setup()
+require("lsp.cmake").setup()
+require("lsp.css").setup()
+require("lsp.docker").setup()
+require("lsp.go").setup()
+require("lsp.html").setup()
+require("lsp.python").setup()
+require("lsp.rust").setup()
+require("lsp.tsserver").setup()
+require("lsp.vim").setup()
+require("lsp.vue").setup()
+require("lsp.yaml").setup()
+require("lsp.sql").setup()
+require("lsp.tex").setup()
+require("lsp.lua").setup()
+require("lsp.tailwindcss").setup()
+require("lsp.emmet").setup()
+require("lsp.solidity").setup()
+
+-- debugger
+require("debugger").setup()
